59838ea99a4c241ca7683c47b62f4ea648fc7b2e
VolodymyrStyk <volodymyr.styk@gmail.com>
2021-04-29 16:27:54 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,47 +1,42 @@
 import './App.css';
-import User from "./components/user/user.component";
 import {useState} from "react";
-
-let usersList = [
-    {name: 'vasya', age: 31, status: false},
-    {name: 'petya', age: 30, status: true},
-    {name: 'kolya', age: 29, status: true},
-    {name: 'olya', age: 28, status: false},
-    {name: 'max', age: 30, status: true},
-    {name: 'anya', age: 31, status: false},
-    {name: 'oleg', age: 28, status: false},
-    {name: 'andrey', age: 29, status: true},
-    {name: 'masha', age: 30, status: true},
-    {name: 'olya', age: 31, status: false},
-    {name: 'max', age: 31, status: true}
-];
 
 
 function App() {
-    let [getUser,setUser] = useState(usersList);
+    let [getCounter, setCounter] = useState(0);
 
-    let deleteFirstUser = () => {
-        getUser.shift();
-        setUser([...getUser]);
+    let increment = () => {
+        getCounter++;
+        setCounter(getCounter);
     }
-    let deleteLastUser = () => {
-        getUser.pop();
-        setUser([...getUser]);
+
+    let decrement = () => {
+        getCounter--;
+        setCounter(getCounter);
     }
-    let deleteCurrentUser = (id) => {
-        getUser.splice(id,1);
-        setUser([...getUser]);
+    let reset = () => {
+        getCounter = 0;
+        setCounter(getCounter);
+    }
+    let increaseStep = () => {
+        getCounter += 5;
+        setCounter(getCounter);
+    }
+    let decreaseStep = () => {
+        getCounter -= 5;
+        setCounter(getCounter);
     }
 
     return (
         <div>
-            <button onClick={deleteFirstUser}>Delete First User</button>
-            <button onClick={deleteLastUser}>Delete last User</button>
-            {
-                getUser.map((value, index) =>
-                    <User key={index} {...value} id = {index}/>
-                )
-            }
+            <div>This is counter: {getCounter}</div>
+            <button onClick={increment}>Increment</button>
+            <button onClick={increaseStep}>Increment + 5</button>
+            <br/>
+            <button onClick={decrement}>Decrement</button>
+            <button onClick={decreaseStep}>Increment - 5</button>
+            <br/>
+            <button onClick={reset}>Reset</button>
         </div>
     );
 }
