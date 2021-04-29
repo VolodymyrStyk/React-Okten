6143e5664160ffba7c84bb9e0e12bda9580373b7
VolodymyrStyk <volodymyr.styk@gmail.com>
2021-04-29 14:15:19 +0300

    update task users

--- src/App.js
+++ src/App.js
@@ -1,12 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import User from "./components/user/user";
+import {useState} from "react";
+
+let usersList = [
+    {name: 'vasya', age: 31, status: false},
+    {name: 'petya', age: 30, status: true},
+    {name: 'kolya', age: 29, status: true},
+    {name: 'olya', age: 28, status: false},
+    {name: 'max', age: 30, status: true},
+    {name: 'anya', age: 31, status: false},
+    {name: 'oleg', age: 28, status: false},
+    {name: 'andrey', age: 29, status: true},
+    {name: 'masha', age: 30, status: true},
+    {name: 'olya', age: 31, status: false},
+    {name: 'max', age: 31, status: true}
+];
+
+
 
 function App() {
-  return (
-    <div className="App">
+    let [users, setUsers] = useState(usersList);
 
-    </div>
-  );
+    const deleteUser = () => {
+        users.pop();
+        console.log('click ');
+        setUsers([...users]);
+    };
+
+    return (
+        <div>
+            {
+                users.map((value, index) =>
+                    <User key={index}
+                          {...value}
+                    />)
+            }
+            <button onClick={deleteUser}>delete User</button>
+        </div>
+    );
 }
 
 export default App;
