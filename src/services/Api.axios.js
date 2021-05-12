fb9698ea54736c371c750615042aaabc832bc207
VolodymyrStyk <volodymyr.styk@gmail.com>
2021-05-13 00:33:54 +0300

    update

--- src/services/Api.axios.js
+++ src/services/Api.axios.js
@@ -1,6 +1,12 @@
-import axios 'axios'
+import axios from 'axios'
 
 const option = {
-    baseURL:"https://jsonplaceholder.typicode.com/";
- }
- const axiosInstace = axios
+    baseURL: "https://jsonplaceholder.typicode.com/";
+}
+const axiosInstace = axios.create(option);
+const getUsers = () => {
+    return axiosInstace.get('/users');
+}
+const getPosts = () =>{
+    return axiosInstace.get('/posts');
+}
